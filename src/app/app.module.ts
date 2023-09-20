906c368b3a6554c4c930727998d3730f449f97ce
lalozuniga <lalozuniga520@gmail.com>
2023-09-20 13:40:59 -0600

    guardado de datos en firebase y formulario completo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import firebase from "firebase/app";
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfHVjTuDWBU1BNfRCP2rgvRP_97ntU0ck",
+  authDomain: "db-api-en-tiempo-real-a1af2.firebaseapp.com",
+  projectId: "db-api-en-tiempo-real-a1af2",
+  storageBucket: "db-api-en-tiempo-real-a1af2.appspot.com",
+  messagingSenderId: "989440459400",
+  appId: "1:989440459400:web:9afd166389fee1c696153d"
+};
+firebase.initializeApp(firebaseConfig);
 
 @NgModule({
   declarations: [AppComponent],
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 //home.page.ts
 import { Component } from '@angular/core';
-
+//import axios from 'axios';
+import firebase from "firebase/app";
+import 'firebase/database';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,7 +12,22 @@ export class HomePage {
   direccion:any;
   colonia:any;
   ciudad:any;
+  nombre:any;
+  apellidos:any;
 
   constructor() {}
+  async guardarFormulario(){
+    const databaseRef = firebase.database().ref('/formulario');
+    const datos = {
+      direccion: this.direccion,
+      nombre: this.nombre,
+      colonia: this.colonia,
+      ciudad: this.ciudad,
+      apellidos: this.apellidos,
+    };
+    databaseRef.child(this.nombre).set(datos).then(()=>{
+    console.log('Subido Exitosamente!')
+  });
+  }
 
 }
