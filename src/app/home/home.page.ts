f548b5f754f1a909125678b3b46ee34bc897b918
Lalozuniga <lalozuniga520@gmail.com>
2023-09-22 08:17:51 -0600

    alertas agregadas

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 //import axios from 'axios';
 import firebase from "firebase/app";
 import 'firebase/database';
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -15,19 +16,44 @@ export class HomePage {
   nombre:any;
   apellidos:any;
 
-  constructor() {}
+
+  constructor(private alertcontroller: AlertController) {}
   async guardarFormulario(){
+    if(this.nombre == '' ||  this.apellidos=='' || this.direccion == '' || this.colonia == '' || this.ciudad == ''){
+      this.mostrarMensaje('te falta un campo de texto', 'mensaje')
+      return;
+    }
+
+
+
+
     const databaseRef = firebase.database().ref('/formulario');
-    const datos = {
-      direccion: this.direccion,
-      nombre: this.nombre,
-      colonia: this.colonia,
-      ciudad: this.ciudad,
-      apellidos: this.apellidos,
-    };
-    databaseRef.child(this.nombre).set(datos).then(()=>{
-    console.log('Subido Exitosamente!')
-  });
+    const idUnico = databaseRef.push().key;//esta linea genera un id inico para que si alguien tiene el mismo nombre no se actualize
+    if (idUnico){
+      const datos = {
+        direccion: this.direccion,
+        nombre: this.nombre,
+        colonia: this.colonia,
+        ciudad: this.ciudad,
+        apellidos: this.apellidos,
+      };
+      databaseRef.child(idUnico).set(datos).then(()=>{
+        //this.mostrarMensaje('mi mensaje' , 'exito!');
+        console.log('Subido Exitosamente!')
+      });
+    }
+  }
+  
+  async mandarmensaje(titulo:string, mensaje:string){
+    const alerta = await this.alertcontroller.create({
+      //titulo
+      //cuerpo
+      //botones
+      header:titulo,
+      message:mensaje,
+      buttons:['cerrar']
+    });
+    await alerta.present();
   }
 
 }
